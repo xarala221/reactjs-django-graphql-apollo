4a8e71bc4dd55fb4ecec75be6c14c0737a4d15bd
= <xaralaxarala@gmail.com>
2019-10-28 15:39:45 +0000

    Finish logout

--- client/src/Root.js
+++ client/src/Root.js
@@ -1,14 +1,32 @@
 import React from "react";
-import withRoot from "./withRoot";
 import { Query } from "react-apollo";
 import { gql } from "apollo-boost";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import withRoot from "./withRoot";
+import App from "./pages/App";
+import Profile from "./pages/Profile";
+import Header from "./components/Shared/Header.js";
+import Loading from "./components/Shared/Loading";
+import Error from "./components/Shared/Loading";
 
 const Root = () => (
-  <Query query={GET_TRACKS_QUERY}>
+  <Query query={ME_QUERY}>
     {({ data, loading, error }) => {
-      if (loading) return <div>Loading</div>;
-      if (error) return <div>error</div>;
-      return <div>{JSON.stringify(data)}</div>;
+      if (loading) return <Loading />;
+      if (error) return <Error error={error.message} />;
+
+      const currentUser = data.me;
+      return (
+        <Router>
+          <>
+            <Header currentUser={currentUser} />
+            <Switch>
+              <Route exact path="/" component={App} />
+              <Route path="/profile/:id" component={Profile} />
+            </Switch>
+          </>
+        </Router>
+      );
     }}
   </Query>
 );
@@ -23,4 +41,13 @@ const GET_TRACKS_QUERY = gql`
     }
   }
 `;
+const ME_QUERY = gql`
+  {
+    me {
+      id
+      username
+    }
+  }
+`;
+
 export default withRoot(Root);
--- client/src/components/Auth/Signout.js
+++ client/src/components/Auth/Signout.js
@@ -1,11 +1,31 @@
 import React from "react";
+import { ApolloConsumer } from "react-apollo";
 import withStyles from "@material-ui/core/styles/withStyles";
-// import ExitToApp from "@material-ui/icons/ExitToApp";
-// import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
+import ExitToApp from "@material-ui/icons/ExitToApp";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 const Signout = ({ classes }) => {
-  return <div>Signout</div>;
+  const handleSignout = client => {
+    localStorage.removeItem("authToken");
+    client.writeData({ data: { isLoggedIn: false } });
+  };
+  return (
+    <ApolloConsumer>
+      {client => (
+        <Button onClick={() => handleSignout(client)}>
+          <Typography
+            variant="body1"
+            className={classes.buttonText}
+            color="secondary"
+          >
+            Signout
+          </Typography>
+          <ExitToApp className={classes.buttonIcon} color="secondary" />
+        </Button>
+      )}
+    </ApolloConsumer>
+  );
 };
 
 const styles = {
